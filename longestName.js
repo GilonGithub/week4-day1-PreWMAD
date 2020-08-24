b5b7b4adeaaa1924131278809396d126ecf1d77c
GilOnGithub <gmjetomo@outlook.com>
2020-08-24 12:43:11 -0700

    longestName.js completed

--- longestName.js
+++ longestName.js
@@ -1,7 +1,33 @@
 // write a function that returns the longest name {first and last} in the given array of objects
 
 const LongestName = function(instructors) {
-  // Put your solution here
+  
+  let firstAndLastName = [];
+  let temp = 0;
+  let i = 0;
+
+  for (let i = 0; i < instructors.length; i++) {
+    
+    firstAndLastName.push(instructors[i].first + " " + instructors[i].last);
+
+  }
+  
+
+    firstAndLastName.forEach(function(elem, index){
+
+      if (temp < elem.length) {
+
+        temp = elem.length;
+        i = index;
+
+      }
+
+    });
+
+  //console.log(firstAndLastName);
+
+  return(firstAndLastName[i]);
+
 };
 
 console.log(LongestName([
@@ -11,7 +37,7 @@ console.log(LongestName([
   {first: "Donald", last: "kant"}
 ]));
 console.log(LongestName([
-  {first: "Matthew", last: "Ebert"},
+  {first: "Matthew", last: "Eberty"},
   {first: "David", last: "John"},
   {first: "Domascus", last: "Anderson"}
 ]));
