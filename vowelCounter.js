c2a1e46737b529d81ab3b0c3aee537bfca9d3e9b
GilOnGithub <gmjetomo@outlook.com>
2020-08-24 12:49:29 -0700

    vowelCounter.js completed

--- vowelCounter.js
+++ vowelCounter.js
@@ -1,7 +1,21 @@
 // write a function that counts the number of vowels in a given string and returns that value
 
 const vowelCounter = function(data) {
-  // Put your solution here
+  
+  let arr = data.split('');
+  let count = 0;
+
+  arr.forEach(function(elem){
+
+    if(elem === 'a' || elem === 'e' || elem === 'i' || elem === 'o' || elem === 'u') {
+
+      count += 1;
+    }
+
+  });
+  
+  return count;
+
 };
 
 console.log(vowelCounter("orange"));
