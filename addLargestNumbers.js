1ee88f4db20af36fc5855c78ef2e1bae749fa67a
GilOnGithub <gmjetomo@outlook.com>
2020-08-24 11:49:24 -0700

    addLargestNumbers completed

--- addLargestNumbers.js
+++ addLargestNumbers.js
@@ -1,7 +1,41 @@
 const addLargestNumbers = function(data) {
-  // Put your solution here
+  
+  let sum = 0;
+  let arrLargestNumbers = [];
+
+  const pickLargest = function (data) {
+    
+    let temp = 0;
+
+    data.forEach(function(elem) {
+
+      if (temp < elem) {
+       
+        temp = elem;
+      
+      }
+
+    });
+
+    data.splice(data.indexOf(temp), 1);
+    
+    arrLargestNumbers.push(temp);
+
+    return temp;
+
+  }
+
+    
+  for (let j = 0; j < 2; j++)
+   {
+     sum += pickLargest(data);
+    // console.log(data.join(' '));
+   }
+
+  console.log(arrLargestNumbers);
+     return sum;
 };
 
-console.log(addLargestNumbers([1, 10]));
-console.log(addLargestNumbers([1, 2, 3]));
+//console.log(addLargestNumbers([1, 10]));
+//console.log(addLargestNumbers([1, 2, 3]));
 console.log(addLargestNumbers([10, 4, 34, 6, 92, 2]));
